7b1782e44f4fec6408f7332445633dcf42a1c14c
Adam Musciano <amusciano@gmail.com>
2018-07-10 21:13:02 -0400

    Added token regexes

--- sniff-paste.py
+++ sniff-paste.py
@@ -3,7 +3,7 @@ import socket
 import re
 import configparser
 
-debug=False
+debug=True
 
 
 config = configparser.ConfigParser()
@@ -19,17 +19,35 @@ conn = pymysql.connect(conf_mysql['Host'],conf_mysql['Username'], conf_mysql['Pa
 cur = conn.cursor() # get a cursor
 
 
+secretRegexes = {
+    "Slack Token": "(xox[p|b|o|a]-[0-9]{12}-[0-9]{12}-[0-9]{12}-[a-z0-9]{32})",
+    "RSA private key": "-----BEGIN RSA PRIVATE KEY-----",
+    "SSH (OPENSSH) private key": "-----BEGIN OPENSSH PRIVATE KEY-----",
+    "SSH (DSA) private key": "-----BEGIN DSA PRIVATE KEY-----",
+    "SSH (EC) private key": "-----BEGIN EC PRIVATE KEY-----",
+    "PGP private key block": "-----BEGIN PGP PRIVATE KEY BLOCK-----",
+    "Facebook Oauth": "[f|F][a|A][c|C][e|E][b|B][o|O][o|O][k|K].*['|\"][0-9a-f]{32}['|\"]",
+    "Twitter Oauth": "[t|T][w|W][i|I][t|T][t|T][e|E][r|R].*['|\"][0-9a-zA-Z]{35,44}['|\"]",
+    "GitHub": "[g|G][i|I][t|T][h|H][u|U][b|B].*[['|\"]0-9a-zA-Z]{35,40}['|\"]",
+    "Google Oauth": "(\"client_secret\":\"[a-zA-Z0-9-_]{24}\")",
+    "AWS API Key": "AKIA[0-9A-Z]{16}",
+    "Heroku API Key": "[h|H][e|E][r|R][o|O][k|K][u|U].*[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}",
+    "Generic Secret": "[s|S][e|E][c|C][r|R][e|E][t|T].*['|\"][0-9a-zA-Z]{32,45}['|\"]"
+}
+
 urlLog= open('out/pastebin-urls.txt','w')
 emailLog = open('out/pastebin-emails.txt','w')
 phoneLog = open('out/pastebin-phone-numbers.txt','w')
 ipLog = open('out/pastebin-ips.txt','w')
+secretsLog = open('out/pastebin-secrets.txt','w')
 
-
-results = cur.execute('select data, link  from pastes;')
 totalIPs=0
 totalURLs=0
 totalPhoneNumbers=0
 totalEmails=0
+totalSecrets=0
+
+results = cur.execute('select data, link  from pastes;')
 
 for row in cur.fetchall():
     ips = re.findall( r'[0-9]+(?:\.[0-9]+){3}', str( row ) )
@@ -77,12 +95,24 @@ for row in cur.fetchall():
         except:
             print("\tInvalid IP")
 
+    
+    secrets = None
+
+    for key, value in secretRegexes.items():
+        secrets = re.findall(value, str(row)) 
+        for secret in secrets:
+           totalSecrets+=1 
+           print("\t"+key+": "+secret)
+           secretsLog.write(key+": "+secret+", https://pastebin.com/" + row[1]+"\n")
+
+
+
 
     if(len(emails) > 0): print("Emails Scraped: "+str(len(emails)))
     if(len(ips) > 0): print("IPs Scraped: "+str(len(ips)))
     if(len(urls) > 0): print("Urls Scraped: "+str(len(urls)))
     if(len(phoneNumbers) > 0): print("Phone Numbers Scraped: "+str(len(phoneNumbers)))
-
+    if(len(secrets) > 0): print("Secrets Scraped: "+str(len(secrets)))
     
     emails = None
     ips = None
@@ -94,7 +124,7 @@ print("IPs:\t\t"+str(totalIPs))
 print("Emails:\t\t"+str(totalEmails))
 print("Urls:\t\t"+str(totalURLs))
 print("Phone #s:\t"+str(totalPhoneNumbers))     
-
+print("Secrets:\t"+str(totalSecrets))     
 
 emailLog.close()
 phoneLog.close()
